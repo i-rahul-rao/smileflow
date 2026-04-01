e5d2f3a9382d2094fe7929ecdd6b717bec818d0c
i-rahul-rao <rk8397971446@gmail.com>
2026-04-01 13:56:29 +0530

    change font-family and colors.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import UserSync from "@/components/UserSync";
 import TanStackProvider from "@/components/providers/TanStackProvider";
 import { Toaster } from "sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const robotoSans = Roboto({
+  variable: "--font-roboto-sans",
   subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
   subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 
 export const metadata: Metadata = {
@@ -42,7 +44,7 @@ export default function RootLayout({
       >
         <html lang="en">
           <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+            className={`${robotoSans.variable} ${robotoMono.variable} antialiased dark`}
           >
             {/* this is done in the home page component */}
             {/* <UserSync /> */}
